fe1df875336279ed1f7356166986313db4b86909
charles adah <charlesadah123@gmail.com>
2023-03-16 19:02:43 -0700

    compilation in c- task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include<stdio.h>
 
 /**
  * main - the main function
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+        return (0);
 }
