452c0d1491212f54498e33cd4c07c11e433d19fe
charles adah <charlesadah123@gmail.com>
2023-03-16 19:36:06 -0700

    five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - this will include a printf statement
+ *
+ * Return: this will also return o
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
