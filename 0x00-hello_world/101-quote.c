335a67224940ff0a8787e1dd86c678292cc7175d
charles adah <charlesadah123@gmail.com>
2023-03-16 21:00:53 -0700

    number 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+
+/**
+ * main - prints wondrfully
+ *
+ * Return: no point giving explanation
+ */
+
+int main(void)
+{
+	write(2, "and tat piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
