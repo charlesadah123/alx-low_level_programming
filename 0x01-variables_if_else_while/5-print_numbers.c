a15eb1ae0092f079a28b525aa433516a9f767081
charles adah <charlesadah123@gmail.com>
2023-03-20 19:22:42 -0700

    hello

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - for char
+ *
+ * Return: must always return 0
+ */
+
+
+int main(void)
+{
+int i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+printf("%d", i);
+putchar('\n');
+return (0);
+}
