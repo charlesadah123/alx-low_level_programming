5a6b783075f60d27ceefdf0103c09df32143eb4b
charles adah <charlesadah123@gmail.com>
2023-03-20 18:51:39 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - for char
+ *
+ * Return: must always return 0
+ */
+
+
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
+
