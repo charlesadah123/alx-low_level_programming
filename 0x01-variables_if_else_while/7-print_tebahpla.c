d609e1517376dbcb7b1abe731eab7989138ec6ee
charles adah <charlesadah123@gmail.com>
2023-03-20 19:40:08 -0700

    hop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - for char
+ *
+ * Return: must always return 0
+ */
+
+
+int main(void)
+{
+char i;
+
+for (i = 'z' ; i < 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
