534ffad169ee06d4be3a27f57a726e31ec132d55
charles adah <charlesadah123@gmail.com>
2023-03-22 13:26:47 -0700

    commitment

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - using putcher to print
+ *
+ * Return: This will always return o
+ *
+ */
+
+int main(void) 
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
