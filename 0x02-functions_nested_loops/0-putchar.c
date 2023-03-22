77920387e0de1a2e7fb03434c2ff180360083c47
charles adah <charlesadah123@gmail.com>
2023-03-22 14:11:02 -0700

    put character

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,7 @@
  * Return: This will always return o
  *
  */
-
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
